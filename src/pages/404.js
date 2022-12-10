2546350e6523fb87d9703ad9d5644970d6258f66
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-10 23:35:11 +0100

    minor update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,31 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import "../components/styles.css"
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export const Head = () => <Seo title="404: Not Found" />
+const NotFoundPage = () => {
+  return (
+    <Layout>
+      <div class="container">
+        <img
+          class="pokemonImage"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png"
+          alt="Pikachu"
+        />
+        <div class="title">
+          Oh no, the page you are looking for cannot be found!
+        </div>
+        <div class="description">
+          It looks like you may have stumbled upon a missing page. Don't worry,
+          just use the back button to return to the previous page, or visit our
+          homepage to start over.
+        </div>
+        <a class="backButton" href="/">
+          Back
+        </a>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
