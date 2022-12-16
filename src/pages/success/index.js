9e92a177126691a9171e909a5724a386c5cbc19e
Raven Van Hove <raven.vanhove@student.ap.be>
2022-12-16 10:27:33 +0100

    style update

--- src/pages/success/index.js
+++ src/pages/success/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from '../../components/layout'
+import "../../components/styles.css"
 
 const Success = () => (
-    <div>
-      <h1>Succes!</h1>
-      <p>Je formulier is succesvol ingediend.</p>
-      <Link to="/">Terug naar de homepage</Link>
-      <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' alt="Pikachu" />
-    </div>
+    <Layout pageTitle="Success">
+      <div className="container">
+          <p className="center">Your question is submitted.<br/>We'll get back to you as fast as possible</p>
+          <Link to="/" className="navItem">Terug naar de homepage</Link>
+          <img src='https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' alt="Pikachu" width={250} />
+      </div>
+    </Layout>
   )
   
   export default Success
